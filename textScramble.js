9f13ea5822eb365e44d14ef1b90c6b2bc82999de
xetrux <147344357+xetrux@users.noreply.github.com>
2024-09-11 13:29:02 +0300

    shit

--- textScramble.js
+++ textScramble.js
@@ -4,40 +4,55 @@ const button2 = document.getElementById("tiedotNappi");
 
 var originalText = [];
 
+var transitioning = false;
+
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function deleteText(Element) {
-    originalText.push(Element.innerHTML);
-    var iterations = Element.innerHTML.length;
+async function deleteText(element) {
+    originalText.push(element.innerHTML);
+    var iterations = element.innerHTML.length;
     for (let i = 0; i < iterations; i++) {
-        await sleep(40 - (Element.innerHTML.length * 3));
-        var newStr = Element.innerHTML.slice(0, -1);
-        Element.innerHTML = newStr;
+        await sleep(40 - (element.innerHTML.length * 3));
+        var newStr = element.innerHTML.slice(0, -1);
+        element.innerHTML = newStr;
     }
 }
 
-async function showText(Element) {
-    var iterations = text.length
+async function showText(element, Text) {
     await sleep(2000);
+    var iterations = Text.length;
     for (let i = 0; i < iterations; i++) {
-        text[i].textContent = originalText[i] 
+        await sleep(30 - (element.innerHTML.length * 2));
+        var currentText = element.innerHTML
+        var newText = currentText + Text.charAt(i);
+        element.textContent = newText;
     }
 }
 
 function callback1() {
+    if (!transitioning) {
+        
+    }
     for (let i = 0; i < text.length; i++) {
-        //console.log(Element)
         deleteText(text[i])
         button.textContent = "[PROJEKTIT]"
         button2.textContent = "TIEDOT"
     }
-    //console.log(originalText);
 }
 
 function callback2() {
-    showText();
+    if (!transitioning) {
+
+    }
+    for (let i = 0; i < text.length; i++) {
+        var orgText = originalText[i];
+        showText(text[i], orgText);
+    }
+    button.textContent = "PROJEKTIT"
+    button2.textContent = "[TIEDOT]"
+
 }
 
 button.addEventListener("click", callback1)
